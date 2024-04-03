da2e0d02604695e9b972cc459807030b8264bca3
Mengjia Lyu <mengjialyu@Mengjias-MBP.home>
2024-04-02 17:22:45 -0700

    use set

--- graph.py
+++ graph.py
@@ -27,35 +27,47 @@ class Graph(abc.ABC):
     def display(self):
         pass
 
-class AdjacencyMatrixGraph(Graph):
+class Node:
+    def __init__(self, vertexId):
+        self.vertexId = vertexId
+        self.adjacency_set = set()
+
+    def add_edge(self, v):
+        if self.vertexId == v:
+            raise ValueError("The vertex %d cannot be adjacent to itself", v)
+
+        self.adjacency_set.add(v)
+
+    def get_adjacent_vertices(self):
+        return sorted(self.adjacency_set)
+
+
+class AdjacencySetGraph(Graph):
 
     def __init__(self, numVertices, directed=False):
-        super(AdjacencyMatrixGraph, self).__init__(numVertices, directed)
+        super(AdjacencySetGraph, self).__init__(numVertices, directed)
 
-        self.matrix = np.zeros((numVertices, numVertices))
+        self.vertex_list = []
+        for i in range(numVertices):
+            self.vertex_list.append(Node(i))
 
     def add_edge(self, v1, v2, weight=1):
         if v1 >= self.numVertices or v2 >= self.numVertices or v1 < 0 or v2 < 0:
             raise ValueError("Vertices %d and %d are out of bounds", v1, v2)
+        
+        if weight != 1:
+            raise ValueError("An adjacency set cannot represent edge weights >1")
 
-        if weight < 1:
-            raise ValueError("An edge cannot have weight < 1")
-
-        self.matrix[v1][v2] = weight
+        self.vertex_list[v1].add_edge(v2)
 
         if self.directed == False:
-            self.matrix[v2][v1] = weight
+            self.vertex_list[v2].add_edge(v1)
 
     def get_adjacent_vertices(self, v):
         if v < 0 or v >= self.numVertices:
             raise ValueError("Cannot access vertex %d", v)
 
-        adjacent_vertices = []
-        for i in range(self.numVertices):
-            if self.matrix[v][i] > 0:
-                adjacent_vertices.append(i)
-
-        return adjacent_vertices
+        return self.vertex_list[v].get_adjacent_vertices()
 
     def get_indegree(self, v):
         if v < 0 or v >= self.numVertices:
@@ -63,26 +75,39 @@ class AdjacencyMatrixGraph(Graph):
 
         indegree = 0
         for i in range(self.numVertices):
-            if self.matrix[v][i] > 0:
-                indegree += 1
+            if v in self.get_adjacent_vertices(i):
+                indegree = indegree + 1
 
         return indegree
 
-    
-
-
     def get_edge_weight(self, v1, v2):
-        return self.matrix[v1][v2]
+        return 1
 
     def display(self):
         for i in range(self.numVertices):
             for v in self.get_adjacent_vertices(i):
                 print(i, "-->", v)
 
+g = AdjacencySetGraph(9, directed=True)
 
-g = AdjacencyMatrixGraph(4)
-
-g.add_edge(0,1)
-g.add_edge(0,2)
-g.add_edge(2,3)
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(2, 7)
+g.add_edge(2, 4)
+g.add_edge(2, 3)
+g.add_edge(1, 5)
+g.add_edge(5, 6)
+g.add_edge(6, 3)
+g.add_edge(3, 4)
+g.add_edge(6, 8)
 g.display()
+
+# 0 -> 1 -> 5 ->  6 -> 8
+#      |
+#      ˇ          |
+#   -> 2 -> 7     |
+#      | 
+#      --> 3 <- - |
+#      |   | 
+#      |   ˇ       
+#      | ->4  
