a0cb6d33b1f52aa4ef22c3192491c4fa93627be7
Mengjia Lyu <mengjialyu@Mengjias-MBP.home>
2024-04-02 20:42:43 -0700

    dfs

--- traversal.py
+++ traversal.py
@@ -21,11 +21,21 @@ def breadth_first(graph, start=0):
             if visited[v] != 1:
                 queue.append(v)
 
+def depth_first(graph, visited, current=0):
+    if visited[current] == 1:
+        return
+
+    visited[current] = 1
+
+    print("Visit: ", current)
+
+    for vertex in graph.get_adjacent_vertices(current):
+        depth_first(graph, visited, vertex)
 
 g = AdjacencySetGraph(9, directed=True)
 
 g.add_edge(0, 1)
-g.add_edge(0, 2)
+g.add_edge(1, 2)
 g.add_edge(2, 7)
 g.add_edge(2, 4)
 g.add_edge(2, 3)
@@ -35,4 +45,5 @@ g.add_edge(6, 3)
 g.add_edge(3, 4)
 g.add_edge(6, 8)
 
-breadth_first(g, 0)
+visited = np.zeros(g.numVertices)
+depth_first(g, visited)
